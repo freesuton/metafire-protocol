57fb97a68a1b5d3d496a16894fd19db17113c672
slowpon <slowpon@outlook.com>
2022-10-22 09:39:26 -0400

    add data types

---
+++ contracts/libraries/types/DataTypes.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: agpl-3.0
+pragma solidity 0.8.17;
+
+library DataTypes {
+  struct ReserveData {
+    //stores the reserve configuration
+    ReserveConfigurationMap configuration;
+    //the liquidity index. Expressed in ray
+    uint128 liquidityIndex;
+    //variable borrow index. Expressed in ray
+    uint128 variableBorrowIndex;
+    //the current supply rate. Expressed in ray
+    uint128 currentLiquidityRate;
+    //the current variable borrow rate. Expressed in ray
+    uint128 currentVariableBorrowRate;
+    uint40 lastUpdateTimestamp;
+    //tokens addresses
+    address bTokenAddress;
+    address debtTokenAddress;
+    //address of the interest rate strategy
+    address interestRateAddress;
+    //the id of the reserve. Represents the position in the list of the active reserves
+    uint8 id;
+  }
+
+  struct NftData {
+    //stores the nft configuration
+    NftConfigurationMap configuration;
+    //address of the bNFT contract
+    address bNftAddress;
+    //the id of the nft. Represents the position in the list of the active nfts
+    uint8 id;
+    uint256 maxSupply;
+    uint256 maxTokenId;
+  }
+
+    struct ReserveConfigurationMap {
+    //bit 0-15: LTV
+    //bit 16-31: Liq. threshold
+    //bit 32-47: Liq. bonus
+    //bit 48-55: Decimals
+    //bit 56: Reserve is active
+    //bit 57: reserve is frozen
+    //bit 58: borrowing is enabled
+    //bit 59: stable rate borrowing enabled
+    //bit 60-63: reserved
+    //bit 64-79: reserve factor
+    uint256 data;
+  }
+
+    struct NftConfigurationMap {
+    //bit 0-15: LTV
+    //bit 16-31: Liq. threshold
+    //bit 32-47: Liq. bonus
+    //bit 56: NFT is active
+    //bit 57: NFT is frozen
+    uint256 data;
+  }
+}
