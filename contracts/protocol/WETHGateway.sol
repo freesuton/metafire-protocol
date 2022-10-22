c9af80e6a2180f60f9957d895300a19d044b8986
slowpon <slowpon@outlook.com>
2022-10-22 08:34:47 -0400

    add WETHGate way interfaces

---
+++ contracts/protocol/WETHGateway.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: agpl-3.0
+pragma solidity 0.8.17;
+
+import {ERC721HolderUpgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC721/utils/ERC721HolderUpgradeable.sol";
+import {IERC721Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC721/IERC721Upgradeable.sol";
+
+
+import {IWETH} from "../interfaces/IWETH.sol";
+import {IWETHGateway} from "../interfaces/IWETHGateway.sol";
+
+contract WETHGateway is IWETHGateway  {
+
+}
