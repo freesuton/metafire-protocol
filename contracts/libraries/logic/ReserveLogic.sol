c78fb730098dd6cf6407b9417b06d2deeced679c
slowpon <slowpon@outlook.com>
2022-12-20 06:30:34 -0500

    debt token

--- contracts/libraries/logic/ReserveLogic.sol
+++ contracts/libraries/logic/ReserveLogic.sol
@@ -12,3 +12,33 @@ import {DataTypes} from "../types/DataTypes.sol";
 
 import {IERC20Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
 import {SafeERC20Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeable.sol";
+
+/**
+ * @title ReserveLogic library
+ * @author Metafire
+ * @notice Implements the logic to update the reserves state
+ */
+library ReserveLogic {
+    using WadRayMath for uint256;
+    using PercentageMath for uint256;
+    using SafeERC20Upgradeable for IERC20Upgradeable;
+
+    /**
+    * @dev Emitted when the state of a reserve is updated
+    * @param asset The address of the underlying asset of the reserve
+    * @param liquidityRate The new liquidity rate
+    * @param variableBorrowRate The new variable borrow rate
+    * @param liquidityIndex The new liquidity index
+    * @param variableBorrowIndex The new variable borrow index
+    **/
+    event ReserveDataUpdated(
+        address indexed asset,
+        uint256 liquidityRate,
+        uint256 variableBorrowRate,
+        uint256 liquidityIndex,
+        uint256 variableBorrowIndex
+    );
+
+    using ReserveLogic for DataTypes.ReserveData;
+    
+}
