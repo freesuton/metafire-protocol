cd72753a645f701ca8a666a2dfcdcacc833534a1
slowpon <slowpon@outlook.com>
2023-01-18 02:53:34 -0500

    update test layout

--- test/configuration.ts
+++ test/configuration.ts
@@ -19,6 +19,7 @@ describe("MetaFire Protocol Deployment", function () {
   let liquidateLogic: any;
   let reserveLogic: any;
   let nftLogic: any;
+  let configuratorLogic: any;
 
   this.beforeEach(async () => {
     
@@ -44,59 +45,24 @@ describe("MetaFire Protocol Deployment", function () {
     const NftLogic = await ethers.getContractFactory("NftLogic");
     nftLogic = await NftLogic.deploy();
 
+    const ConfiguratorLogic = await ethers.getContractFactory("ConfiguratorLogic");
+    configuratorLogic = await ConfiguratorLogic.deploy();
+
   })
 
-  describe("Proxy Configuation", async function () {
 
-    it("Set Proxy Admin", async function () {
-      const [owner, addr1] = await ethers.getSigners();
 
-      // Deploy Lend Pool
-      const LendPool = await ethers.getContractFactory("LendPool", {
+
+
+
+  describe("Init Reserve and NFT", async function () {
+    it("", async function () {
+      const LendPoolConfigurator = await ethers.getContractFactory("LendPoolConfigurator", {
         libraries: {
-          SupplyLogic: supplyLogic.address,
-          BorrowLogic: borrowLogic.address,
-          LiquidateLogic: liquidateLogic.address,
-          ReserveLogic: reserveLogic.address,
-          NftLogic: nftLogic.address
+          ConfiguratorLogic: configuratorLogic.address,
         },
       });
-      const lendPool = await LendPool.deploy();
-      await lendPool.deployed();
-
-      // Deploy Proxy
-      const MetaFireUpgradeableProxy = await ethers.getContractFactory("MetaFireUpgradeableProxy");
-      const metaFireUpgradeableProxy = await MetaFireUpgradeableProxy.deploy(lendPool.address, owner.address,'0x');
-      await metaFireUpgradeableProxy.deployed();
-
-      // Deploy Proxy Admin
-      const MetaFireProxyAdmin = await ethers.getContractFactory("MetaFireProxyAdmin");
-      const metaFireProxyAdmin = await MetaFireProxyAdmin.deploy();
-
-      let admin  = await metaFireUpgradeableProxy.callStatic.admin();
-      expect(admin).to.equal(owner.address);
-
-      await metaFireUpgradeableProxy.changeAdmin(metaFireProxyAdmin.address);
-      admin =  await metaFireProxyAdmin.getProxyAdmin(metaFireUpgradeableProxy.address);
-      expect(admin).to.equal(metaFireProxyAdmin.address);
-      // console.log("admin Address: "+ admin1);
-    });
-  })
-
-  describe("Init Lend Pool", async function () {
-    it("Set Proxy Admin", async function () {
-      const LendPoolAddressesProvider = await ethers.getContractFactory("LendPoolAddressesProvider");
-      const lendPoolAddressesProvider = await LendPoolAddressesProvider.deploy("eth");
-
-      const LendPoolAddressesProviderRegistry = await ethers.getContractFactory("LendPoolAddressesProviderRegistry");
-      const lendPoolAddressesProviderRegistry = await LendPoolAddressesProviderRegistry.deploy();
-
-      await lendPoolAddressesProviderRegistry.registerAddressesProvider(lendPoolAddressesProvider.address, 1);
-      const registeredProvider = await lendPoolAddressesProviderRegistry.getAddressesProvidersList();
-      console.log(registeredProvider[0]);
-      expect(registeredProvider[0]).to.equal(lendPoolAddressesProvider.address);
+      const lendPoolConfigurator = await LendPoolConfigurator.deploy();
     })
   })
-
-
 })
--- test/deployment.ts
+++ test/deployment.ts
@@ -9,6 +9,7 @@ import { ContractFunction } from "hardhat/internal/hardhat-network/stack-traces/
 describe("Lend Protocol", function () {
   console.log("------start test -------");
   const oneEther = ethers.BigNumber.from("1000000000000000000");
+  const ray = ethers.BigNumber.from("1000000000000000000000000000");
 
   const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
   const ONE_GWEI = 1_000_000_000;
@@ -19,6 +20,7 @@ describe("Lend Protocol", function () {
   let liquidateLogic: any;
   let reserveLogic: any;
   let nftLogic: any;
+  let configuratorLogic: any;
 
   this.beforeEach(async () => {
     
@@ -44,12 +46,14 @@ describe("Lend Protocol", function () {
     const NftLogic = await ethers.getContractFactory("NftLogic");
     nftLogic = await NftLogic.deploy();
 
+    const ConfiguratorLogic = await ethers.getContractFactory("ConfiguratorLogic");
+    configuratorLogic = await ConfiguratorLogic.deploy();
+
   })
 
-  describe("Deployment", async function () {
+  describe("Deploy and Init LendPool", async function () {
 
     it("Deploy LendPool", async function () {
-      
       const LendPool = await ethers.getContractFactory("LendPool", {
         libraries: {
           SupplyLogic: supplyLogic.address,
@@ -65,6 +69,24 @@ describe("Lend Protocol", function () {
       console.log("LendPool Address: "+ lendPool.address);
     });
 
+    it("Init LendPool", async function () {
+      const LendPoolAddressesProvider = await ethers.getContractFactory("LendPoolAddressesProvider");
+      const lendPoolAddressesProvider = await LendPoolAddressesProvider.deploy("eth");
+      const LendPool = await ethers.getContractFactory("LendPool", {
+        libraries: {
+          SupplyLogic: supplyLogic.address,
+          BorrowLogic: borrowLogic.address,
+          LiquidateLogic: liquidateLogic.address,
+          ReserveLogic: reserveLogic.address,
+          NftLogic: nftLogic.address
+        },
+      });
+      const lendPool = await LendPool.deploy();
+      await lendPool.initialize(lendPoolAddressesProvider.address);
+    })
+  })
+
+  describe("Deploy and Init Proxy contracts", async function () {
     it("Deploy Proxy", async function () {
       const [owner, addr1] = await ethers.getSigners();
       const LendPool = await ethers.getContractFactory("LendPool", {
@@ -84,17 +106,94 @@ describe("Lend Protocol", function () {
       
       console.log("Proxy Address: "+ metaFireUpgradeableProxy.address);
     })
+
+
+    it("Set Proxy Admin", async function () {
+      const [owner, addr1] = await ethers.getSigners();
+
+      // Deploy Lend Pool
+      const LendPool = await ethers.getContractFactory("LendPool", {
+        libraries: {
+          SupplyLogic: supplyLogic.address,
+          BorrowLogic: borrowLogic.address,
+          LiquidateLogic: liquidateLogic.address,
+          ReserveLogic: reserveLogic.address,
+          NftLogic: nftLogic.address
+        },
+      });
+      const lendPool = await LendPool.deploy();
+      await lendPool.deployed();
+
+      // Deploy Proxy
+      const MetaFireUpgradeableProxy = await ethers.getContractFactory("MetaFireUpgradeableProxy");
+      const metaFireUpgradeableProxy = await MetaFireUpgradeableProxy.deploy(lendPool.address, owner.address,'0x');
+      await metaFireUpgradeableProxy.deployed();
+
+      // Deploy Proxy Admin
+      const MetaFireProxyAdmin = await ethers.getContractFactory("MetaFireProxyAdmin");
+      const metaFireProxyAdmin = await MetaFireProxyAdmin.deploy();
+
+      let admin  = await metaFireUpgradeableProxy.callStatic.admin();
+      expect(admin).to.equal(owner.address);
+
+      await metaFireUpgradeableProxy.changeAdmin(metaFireProxyAdmin.address);
+      admin =  await metaFireProxyAdmin.getProxyAdmin(metaFireUpgradeableProxy.address);
+      expect(admin).to.equal(metaFireProxyAdmin.address);
+      // console.log("admin Address: "+ admin1);
+    });
   })
 
-  it("Deploy Address Provider", async function () {
-    const LendPoolAddressesProvider = await ethers.getContractFactory("LendPoolAddressesProvider");
-    const lendPoolAddressesProvider = await LendPoolAddressesProvider.deploy("eth");
-    console.log("Address Provider Address: "+ lendPoolAddressesProvider.address);
+  describe("Deploy and Init Address Provider", async function () {
+    it("Deploy Address Provider Contract", async function () {
+      const LendPoolAddressesProvider = await ethers.getContractFactory("LendPoolAddressesProvider");
+      const lendPoolAddressesProvider = await LendPoolAddressesProvider.deploy("eth");
+      console.log("Address Provider Address: "+ lendPoolAddressesProvider.address);
+    })
+
+    it("Deploy Address Provider Registry Contract", async function () {
+      const LendPoolAddressesProviderRegistry = await ethers.getContractFactory("LendPoolAddressesProviderRegistry");
+      const lendPoolAddressesProviderRegistry = await LendPoolAddressesProviderRegistry.deploy();
+      console.log("Address Provider Registry Address: "+ lendPoolAddressesProviderRegistry.address);
+    })
+
+    it("Register Address Provider", async function () {
+      const LendPoolAddressesProvider = await ethers.getContractFactory("LendPoolAddressesProvider");
+      const lendPoolAddressesProvider = await LendPoolAddressesProvider.deploy("eth");
+
+      const LendPoolAddressesProviderRegistry = await ethers.getContractFactory("LendPoolAddressesProviderRegistry");
+      const lendPoolAddressesProviderRegistry = await LendPoolAddressesProviderRegistry.deploy();
+
+      await lendPoolAddressesProviderRegistry.registerAddressesProvider(lendPoolAddressesProvider.address, 1);
+      const registeredProvider = await lendPoolAddressesProviderRegistry.getAddressesProvidersList();
+      console.log(registeredProvider[0]);
+      expect(registeredProvider[0]).to.equal(lendPoolAddressesProvider.address);
+    })
   })
 
-  it("Deploy Address Provider Registry", async function () {
-    const LendPoolAddressesProviderRegistry = await ethers.getContractFactory("LendPoolAddressesProviderRegistry");
-    const lendPoolAddressesProviderRegistry = await LendPoolAddressesProviderRegistry.deploy();
-    console.log("Address Provider Registry Address: "+ lendPoolAddressesProviderRegistry.address);
+  describe("Deploy and Init Configurator and Interest Strategy", async function () {
+    it("Deploy Interest Rate Strategy Contract", async function () {
+      const LendPoolAddressesProvider = await ethers.getContractFactory("LendPoolAddressesProvider");
+      const lendPoolAddressesProvider = await LendPoolAddressesProvider.deploy("eth");
+  
+      const InterestRate = await ethers.getContractFactory("InterestRate");
+      const interestRate = await InterestRate.deploy(lendPoolAddressesProvider.address,ray.div(100).mul(65),ray.div(100).mul(8),ray.div(100).mul(10),ray);
+      console.log("Interest rate Strategy contract Address: "+ interestRate.address);
+    })
+  
+    it("Deploy LendPoolConfigurator Contract", async function () {
+      const LendPoolConfigurator = await ethers.getContractFactory("LendPoolConfigurator", {
+        libraries: {
+          ConfiguratorLogic: configuratorLogic.address,
+        },
+      });
+      const lendPoolConfigurator = await LendPoolConfigurator.deploy();
+      console.log("LendPoolConfigurator contract Address: "+ lendPoolConfigurator.address);
+    })
   })
+
+
+
+
+
+
 })
