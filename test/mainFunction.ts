9e3401cdb8e2e36185e8b96070c686021654cbb7
slowpon <slowpon@outlook.com>
2023-02-14 16:57:59 -0500

    update types

--- test/mainFunction.ts
+++ test/mainFunction.ts
@@ -4,8 +4,10 @@ import { expect } from "chai";
 import { ethers } from "hardhat";
 import { ContractFunction } from "hardhat/internal/hardhat-network/stack-traces/model";
 import { any } from "hardhat/internal/core/params/argumentTypes";
-
-
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import {WETH9Mocked,MockMetaFireOracle, MockNFTOracle, MockReserveOracle, MintableERC721} from "../typechain-types/contracts/mock";
+import {SupplyLogic,BorrowLogic, LiquidateLogic, ReserveLogic,ConfiguratorLogic} from "../typechain-types/contracts/libraries/logic"
+import {LendPool,LendPoolLoan,LendPoolConfigurator,LendPoolAddressesProvider, InterestRate,MToken, DebtToken} from "../typechain-types/contracts/protocol"
 
 describe("MetaFire Protocol Main Functions", async function () {
   console.log("------start test -------");
@@ -16,38 +18,38 @@ describe("MetaFire Protocol Main Functions", async function () {
   const ONE_MONTH = 3600 * 24 * 30;
   const ONE_GWEI = 1_000_000_000;
 
-  let owner: any;
-  let addr1: any;
-  let addr2: any;
+  let owner: SignerWithAddress;
+  let addr1: SignerWithAddress;
+  let addr2: SignerWithAddress;
 
-  let erc20Assets: any;
-  let nftAssets: any;
+  let erc20Assets: SignerWithAddress[];
+  let nftAssets: SignerWithAddress[];
   
   // Libraries
   let validationLogic: any;
-  let supplyLogic: any;
-  let borrowLogic: any;
-  let liquidateLogic: any;
-  let reserveLogic: any;
+  let supplyLogic: SupplyLogic;
+  let borrowLogic: BorrowLogic;
+  let liquidateLogic: LiquidateLogic;
+  let reserveLogic: ReserveLogic;
   let nftLogic: any;
-  let configuratorLogic: any;
+  let configuratorLogic: ConfiguratorLogic;
 
   // Contracts
   let metaFireUpgradeableProxy: any;
   let metaFireProxyAdmin: any;
-  let lendPool: any;
-  let lendPoolLoan: any;
-  let lendPoolConfigurator: any;
-  let lendPoolAddressesProvider: any;
-  let interestRate: any;
-  let wETH: any;
-  let mTokenimpl: any;
-  let debtTokenImpl: any;
-  let mintableERC721: any;
+  let lendPool: LendPool;
+  let lendPoolLoan: LendPoolLoan;
+  let lendPoolConfigurator: LendPoolConfigurator;
+  let lendPoolAddressesProvider: LendPoolAddressesProvider;
+  let interestRate: InterestRate;
+  let wETH: WETH9Mocked;
+  let mTokenimpl: MToken;
+  let debtTokenImpl: DebtToken;
+  let mintableERC721: MintableERC721;
   let bNFT: any;
   let bNFTRegistry: any;
-  let mockNFTOracle: any;
-  let mockReserveOracle: any;
+  let mockNFTOracle: MockNFTOracle;
+  let mockReserveOracle: MockReserveOracle;
 
 
 
@@ -288,9 +290,9 @@ describe("MetaFire Protocol Main Functions", async function () {
       await lendPool.connect(addr1).auction(mintableERC721.address, 0, oneEther, addr1.address);
       let auctionData = await lendPool.getNftAuctionData(mintableERC721.address, 0);
       let nftAuctionEndTime = await lendPool.getNftAuctionEndTime(mintableERC721.address, 0);
-      console.log("------");
-      console.log(auctionData);
-      console.log(nftAuctionEndTime);
+      // console.log("------");
+      // console.log(auctionData);
+      // console.log(nftAuctionEndTime);
 
       await ethers.provider.send("evm_increaseTime", [3600*24*2]);
       await ethers.provider.send("evm_mine");
