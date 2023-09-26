e529ee4c070b6c0a8d9d4e546a6c2c8cfa6b4e9e
slowpon <slowpon@outlook.com>
2023-09-26 08:15:43 -0400

    op

--- contracts/protocol/NFTLinkOracleGetter.sol
+++ contracts/protocol/NFTLinkOracleGetter.sol
@@ -29,13 +29,16 @@ contract NFTLinkOracleGetter is INFTOracleGetter, Initializable{
      * @param asset of the NFT Oralce
      */
     function getAssetPrice( address asset) override external view returns ( uint256 ){
+        const oracleAddress = _oracleAddresses[asset];
+        require( oracleAddress != address(0), "NFTOracleGetter: Oracle address is not set");
         (
             /*uint80 roundID*/,
             int nftFloorPrice,
             /*uint startedAt*/,
             /*uint timeStamp*/,
             /*uint80 answeredInRound*/
-        ) = AggregatorV3Interface( asset).latestRoundData();
+        ) = AggregatorV3Interface( oracleAddress ).latestRoundData();
+
         require(nftFloorPrice > 0, "NFTOracleGetter: NFT price is 0 or less than 0");
         return uint256(nftFloorPrice);
     }
