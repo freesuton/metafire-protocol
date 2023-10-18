d9d029a804749f9094b38065a324a78eda9a72fe
slowpon <slowpon@outlook.com>
2023-10-18 07:41:44 -0400

    whitelist tasks

--- tasks/protocolOperation/whiteListNFT.ts
+++ tasks/protocolOperation/whiteListNFT.ts
@@ -15,7 +15,7 @@ function saveJsonFile(filename:string, data:any) {
     fs.writeFileSync(filename, jsonString, 'utf-8');
 }
 
-task("whitelist-nft", " add nft asset to the whitelist")
+task("whitelist", " add nft asset to the whitelist")
   .addParam("nftaddress", "The address of the nft asset")
   .setAction(async ( taskArgs , hre) => {
 
@@ -60,7 +60,7 @@ task("register-nft", " register nft asset to the registry")
   .setAction(async ( taskArgs , hre) => {
 
     // Load logic address
-    const path = '../deploys/mainnetContractAddresses.json'
+    const path = './tasks/deploys/mainnetContractAddresses.json';
     const jsonData = loadJsonFile(path);
 
     const nftAssets = [taskArgs.nftaddress];
@@ -70,4 +70,36 @@ task("register-nft", " register nft asset to the registry")
 
     const tx = await bNFTRegistry.createBNFT(taskArgs.nftaddress);
     console.log(tx);
+});
+
+task("init-registered-nft", "init registered nft asset to the registry")
+  .addParam("nftaddress", "The address of the nft asset")
+  .setAction(async ( taskArgs , hre) => {
+
+    // Load logic address
+    const path = './tasks/deploys/mainnetContractAddresses.json';
+    const jsonData = loadJsonFile(path);
+
+    const nftAssets = [taskArgs.nftaddress];
+
+    const LendPoolConfigurator = await hre.ethers.getContractFactory("LendPoolConfigurator", {
+      libraries: {
+        ConfiguratorLogic: jsonData.configuratorLogicAddress,
+      },
+    });
+    const lendPoolConfigurator = LendPoolConfigurator.attach(jsonData.lendPoolConfiguratorProxyAddress);
+    // init NFT
+    const initNftInput: any = [[taskArgs.nftaddress]];
+    await lendPoolConfigurator.batchInitNft(initNftInput);
+    // // 1% -> 100     address, ltv, liquidationThreshold, liquidationBonus(auction), liquidatingBuyBonus
+    await lendPoolConfigurator.configureNftAsCollateral(nftAssets, 6000, 8000, 1000, 500);
+
+    // //set max limit
+    await lendPoolConfigurator.setNftMaxSupplyAndTokenId(nftAssets,100000,100000);
+
+    const WETHGateway = await hre.ethers.getContractFactory("WETHGateway");
+    const wETHGateway = WETHGateway.attach(jsonData.wETHGatewayV2Address);
+
+    // give approval
+    await wETHGateway.approveNFTTransfer(taskArgs.nftaddress, true);
 });
