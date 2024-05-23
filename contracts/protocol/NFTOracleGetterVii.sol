dba8a30beb1fc678963342812fde59e05e16dc16
slowpon <slowpon@outlook.com>
2024-05-23 14:33:57 -0400

    init oracle

--- contracts/protocol/NFTOracleGetterVii.sol
+++ contracts/protocol/NFTOracleGetterVii.sol
@@ -8,40 +8,33 @@ import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Ini
 import {ILendPoolAddressesProvider} from "../interfaces/ILendPoolAddressesProvider.sol";
 import {Errors} from "../libraries/helpers/Errors.sol";
 
-contract NFTLinkOracleGetter is INFTOracleGetter, Initializable{
+contract NFTLinkOracleGetter is INFTOracleGetter, Initializable {
 
     ILendPoolAddressesProvider internal _addressesProvider;
-
-    // NFT Address => Chainlink Oracle Address
-    address _oracleAddresses;
+    INFTOracle internal _nftOracle;
 
     modifier onlyPoolAdmin() {
         require(_addressesProvider.getPoolAdmin() == msg.sender, Errors.CALLER_NOT_POOL_ADMIN);
         _;
     }
 
-    function initialize(ILendPoolAddressesProvider provider) public initializer {
+    function initialize(ILendPoolAddressesProvider provider, address oracleAddress) public initializer {
         _addressesProvider = provider;
+        _nftOracle = INFTOracle(oracleAddress);
     }
 
-    /* CAUTION: Price uint is ETH based (WEI, 18 decimals) */
+    /* CAUTION: Price unit is ETH based (WEI, 18 decimals) */
     /***********
 
     /**
-     * @dev Get NFT floor price from Oralce
-     * @param asset of the NFT Oralce 
+     * @dev Get NFT floor price from Oracle
+     * @param asset of the NFT Oracle 
      */
-    function getAssetPrice( address asset) override external view returns ( uint256 ){
-        // address oracleAddress = _oracleAddresses[asset];
-        require( oracleAddress != address(0), "NFTOracleGetter: Oracle address is not set");
+    function getAssetPrice(address asset) override external view returns (uint256) {
+        require(asset != address(0), "NFTOracleGetter: Oracle address is not set");
   
-        uint256 nftFloorPrice = INFTOracle( oracleAddress ).getAssetPrice(asset);
-
+        uint256 nftFloorPrice = _nftOracle.getAssetPrice(asset);
         require(nftFloorPrice > 0, "NFTOracleGetter: NFT price is 0 or less than 0");
-        return uint256(nftFloorPrice);
-    }
-
-    function setOracle(address oracleAddress) external onlyPoolAdmin {
-        _oracleAddresses = oracleAddress;
+        return nftFloorPrice;
     }
 }
